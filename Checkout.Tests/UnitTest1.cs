eb6fb896d8ad25d50986a2af61aaa4da1db6e7ae
StephanieK <stephanie.kitchen@serko.com>
2019-08-13 15:48:51 +1200

    First test passing, returns 0 for empty string

--- Checkout.Tests/UnitTest1.cs
+++ Checkout.Tests/UnitTest1.cs
@@ -6,9 +6,21 @@ namespace Checkout.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1 ()
+        public void GivenAnEmptyCart_WhenCalculatingTotal_Return0 ()
         {
+            var cart = string.Empty;
+            var checkout = new Scanner();
+            var expectedTotal = checkout.TotalPrice(cart);
 
+            Assert.Equal(0, expectedTotal);
+        }
+    }
+
+    public class Scanner
+    {
+        public int TotalPrice(string cart)
+        {
+            return 0;
         }
     }
 }
